cb24245f5c48d7a6ff2fb3d8105f142740a19536
markdivanov <92439395+markdivanov@users.noreply.github.com>
2021-11-02 19:13:18 +0200

    Add project files.

---
+++ PriceScraper/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using CsvHelper;
+using HtmlAgilityPack;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace FTaskFanning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("https://www.emag.bg/");
+            foreach (var item in doc.DocumentNode.SelectNodes("//p[@class='product-new-price']"))
+            {
+                Console.WriteLine(item.InnerText);
+            }
+            Console.ReadLine();
+        }
+    }
+}
